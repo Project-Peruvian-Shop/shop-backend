d46e5c88f62a8407503476c24b7ea8ec2ec50546
tineojf <92743762+tineodev@users.noreply.github.com>
2025-11-18 10:05:33 -0500

    remove unused profile controllers, routes, and various validators

--- src/validators/friend/createRequestFrienValidator.ts
+++
@@ -1,9 +0,0 @@
-import z from "zod";
-
-export const createRequestFriendSchema = z.object({
-  profileId: z.string().uuid({
-    message: "friendId must be a valid UUID",
-  }),
-});
-
-export type CreateRequestFriendDTO = z.infer<typeof createRequestFriendSchema>;
